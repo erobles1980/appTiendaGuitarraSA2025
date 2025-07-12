7f604c487781bcc10dcb1fd7e1b2f21491458adf
erobles1980 <eroblesp@est.ups.edu.ec>
2025-07-12 09:04:22 -0500

    segundo envio

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,29 @@ import { db } from './db'
 
 export default function App() {
   
-   const [data,setData]=useState(db)
+   const [data]=useState(db)
    const [car, setCar]=useState([])
 
     function addToCart(item){
-        const itemExist=car.findIndex(guitarra => guitarra.id===item.id)
+        const itemExist=car.findIndex(guitarra => guitarra.id === item.id)
         if (itemExist>=0){
             //haremos una copia del carrito
             //console.log("Ya existe")
-            const updateCart=[...cart]
+            const updateCart=[...car]
             updateCart[itemExist].quantity++
-            setCart(updateCart)
+            setCar(updateCart)
         }else{
             //console.log("No existe")
             item.quantity=1
-            setCart(prevCart => [...prevCart, item])
+            setCar(prevCart => [...prevCart, item])
         }
     }
 
   return (
     <>
-        <Header/>
+        <Header
+            car={car}
+        />
         <main className="container-xl mt-5">
             <h2 className="text-center">Nuestra Colección</h2>
 
--- src/Components/Guitarra.jsx
+++ src/Components/Guitarra.jsx
@@ -12,7 +12,7 @@ export default function Guitarra({guitarra,addToCart}){
                     <button 
                         type="button"
                         className="btn btn-dark w-100"
-                        onClick={addToCart}
+                        onClick={() => addToCart(guitarra)}
                     >Agregar al Carrito</button>
                 </div>
             </div>
